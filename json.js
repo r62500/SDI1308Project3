93ff5ae7bd562dd22a0acccbb796c6c1d270dec4
Aaron Korpa <aaronkorpa@Aarons-MacBook-Pro.local>
2013-08-21 18:10:34 -0700

    Created a JSON that contains info about the characters in the story for project 3.

--- json.js
+++ json.js
@@ -1,1 +1,28 @@
 // This is where I will store my JSON data.
+
+var characterData = {
+
+	"characters": [
+	
+		{
+			"characterName":       "Chachi",
+			"characterType":       "Cat",
+			"characterAge":        "13",
+			"characterOccupation": "Housecat",
+		},
+		{
+			"characterName":       "Aaron"
+			"characterType":       "Human"
+			"characterAge":        "31"
+			"characterOccupation": "Audio Engineer and College Student"
+		},
+		{
+			"characterName":       "Carolyn Morrill"
+			"characterType":       "Human"
+			"characterAge":        "Unknown"
+			"characterOccupation": "Veterinarian"
+		}
+	
+	]
+
+};
