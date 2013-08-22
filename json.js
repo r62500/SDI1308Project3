928db1dc91daba7f7cbc36b3d949f88e630e984f
Aaron Korpa <aaronkorpa@Aarons-MacBook-Pro.local>
2013-08-21 18:16:10 -0700

    Fixed errors on json so that it now works properly.

--- json.js
+++ json.js
@@ -8,18 +8,18 @@ var characterData = {
 			"characterName":       "Chachi",
 			"characterType":       "Cat",
 			"characterAge":        "13",
-			"characterOccupation": "Housecat",
+			"characterOccupation": "Housecat"
 		},
 		{
-			"characterName":       "Aaron"
-			"characterType":       "Human"
-			"characterAge":        "31"
+			"characterName":       "Aaron",
+			"characterType":       "Human",
+			"characterAge":        "31",
 			"characterOccupation": "Audio Engineer and College Student"
 		},
 		{
-			"characterName":       "Carolyn Morrill"
-			"characterType":       "Human"
-			"characterAge":        "Unknown"
+			"characterName":       "Carolyn Morrill",
+			"characterType":       "Human",
+			"characterAge":        "Unknown",
 			"characterOccupation": "Veterinarian"
 		}
 	
